f913acdce0be9c27ec452367258bedfebf03baf9
Anahi Bravo <anahi.bravo@protonmail.com>
2022-04-25 23:00:02 -0700

    second codewar of the day

---
+++ april2502.js
@@ -0,0 +1,9 @@
+function solution(str){
+    //   return str.split('').reverse().join('')
+      
+      let newStr = ''
+      for(let i = str.length - 1; i >= 0; i--){
+        newStr += str[i]
+      }
+      return newStr
+    }
