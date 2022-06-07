7047817fc48c538c434498cf7744723025e3799a
Anahi Bravo <anahi.bravo@protonmail.com>
2022-06-07 11:22:16 -0700

    june 7 codewar

---
+++ june72022.js
@@ -0,0 +1,17 @@
+function billboard(name, price = 30){
+    let cost = 0
+    for (let i = 0; i < name.length; i++){
+      cost += price 
+      
+    }
+      return cost
+    } 
+    
+    
+    /*
+    I can't use the * hahaa but would this have worked otherwise?
+    const value = 0
+      let cost = name.reduce((acc,c) => acc * c, value)
+    console.log (cost)
+    let sum = nums.reduce( (acc, c) => acc + c , 0)
+    */
