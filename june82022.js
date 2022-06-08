72326832766ac8cd169e645f73ff95b1a396e3db
Anahi Bravo <anahi.bravo@protonmail.com>
2022-06-08 07:40:09 -0700

    june 8 codewar

---
+++ june82022.js
@@ -0,0 +1,9 @@
+function sayHello(name) {
+  //made each one into a variable to be able to concat together using .concat
+    const stringOne = 'Hello,'
+    const stringTwo = stringOne.concat(' ', `${name}`);
+   //asked to return stringTwo
+    return stringTwo
+  }
+
+
