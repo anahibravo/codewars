bb1bdaefe451141c90a2cf3ab64612758b02b6f1
Anahi Bravo <anahi.bravo@protonmail.com>
2022-06-09 20:59:18 -0600

    june 9 codewar, did not pass it

---
+++ june92022.js
@@ -0,0 +1,5 @@
+function toCsvText(array) {
+    return array.join('\n');
+ }
+
+ // I did not understand at first what the /n meant.... I did not pass this codewar it took me way too long. After seeing the solutions I went to reserach on \n on the mdn
