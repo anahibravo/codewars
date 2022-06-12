1c2b1b7c0e55bca81b20b1dcaebaddb8517e4aea
Anahi Bravo <anahi.bravo@protonmail.com>
2022-06-11 19:00:14 -0600

    2nd code war for june 11

---
+++ june112022_2.js
@@ -0,0 +1,20 @@
+function rowSumOddNumbers(n) {
+	return n * n * n
+}
+
+/* This first attempt I created this but I think it wasn't enough. 
+
+//create a variable that hold the value of the sum of each row
+  const sum = 0
+  //iterate through each element then add them
+  for (let i = 1; i > n.length; i++){
+    sum += n[i];
+  }
+  return sum
+
+// I also was thinking of Math.pow
+function rowSumOddNumbers(n) {
+  return Math.pow(n, 3);
+}
+
+  */
