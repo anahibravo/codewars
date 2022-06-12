a30f0d837f166ac9087baa0fe6602e4defee8d22
Anahi Bravo <anahi.bravo@protonmail.com>
2022-06-11 18:27:03 -0600

    june 11 codewar

---
+++ june112022.js
@@ -0,0 +1,5 @@
+// Write the "square"-function here
+function square (num) {
+    return num * num
+  }
+  //I don't like it when your code doesn't pass because you named it something different. Like really? does the name of the function really matter? Mine was squareNum like it still has the word square in it. It's just annoying sometimes. 
