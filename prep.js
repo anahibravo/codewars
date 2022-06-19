7a81d8df6269c57839e4d77db53b82f6a25f7fa6
Anahi Bravo <anahi.bravo@protonmail.com>
2022-06-18 23:21:33 -0700

    june 18 codewar

---
+++ prep.js
@@ -0,0 +1,20 @@
+//PREP
+// 1 to some num, 3 fizz, 5 buzz, 3 and 5 fizzbuzz 
+// P: I have some question about this number... will it always be a number? no boolean
+//will it always be a whole? will it always be positive? Any other infomration about this number?
+//R: you want me to return or console log this value?
+//E: Do you mind if I give you an example first? It's Fizz Buzz right? ok thank you
+
+function fizzBuzz(num){
+
+}
+
+fizzBuzz(5) // 1,2,fizz,4,fizz
+fizzBuzz(3) // 1,2,fizz
+fizzBuzz(15) // 1,2,fizz,4,fizz,buzz,fizz,7,8,fizz,buzz,11,fizz,13,14,fizzBuzz
+//do these test cases look fine? Do they look like they fit
+
+//P:  
+  // loop
+ //conditionals that use %
+ //console.log num or fizz,buzzmfuzzBuzz
