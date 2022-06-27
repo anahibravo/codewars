057514cf26c753eaab83073f76d9319b9feaa42b
Anahi Bravo <anahi.bravo@protonmail.com>
2022-06-26 19:19:21 -0700

    june 26 codewar

---
+++ june262022.js
@@ -0,0 +1,5 @@
+function greet() {
+    return "hello world!";
+    }
+
+    //I got got with this codewar I did not use the greet I created my own and it wasn't passing the tests. I have done this before and I didn't realize it. 
