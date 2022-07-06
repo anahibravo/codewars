fea654138be33d174040d339d8f7398458442c02
Anahi Bravo <anahi.bravo@protonmail.com>
2022-07-05 22:59:44 -0700

    july 5 codewar

---
+++ july52022.js
@@ -0,0 +1,10 @@
+function correctTail(bod, tail){
+  
+    let sub = bod.substr(bod.length - tail.length)
+    
+    if (sub === tail) {
+      return true
+    }
+    else 
+      return false
+    }
