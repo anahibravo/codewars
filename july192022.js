6c7487ef5c158a4429d905c175f894ce5d95443d
Anahi Bravo <anahi.bravo@protonmail.com>
2022-07-19 23:12:57 -0700

    july 19 codewar

---
+++ july192022.js
@@ -0,0 +1,6 @@
+function isLockNessMonster(s) {
+    return s.includes("tree fiddy") || s.includes("3.50");
+  }
+
+  //this was a hard one I did not understand until I saw the solutions
+  //this solution was my favorite 
