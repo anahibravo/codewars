f3257de508b218a4b2b9c1fe2c0d8719abf7eb53
Anahi Bravo <anahi.bravo@protonmail.com>
2022-07-25 15:09:22 -0700

    july 25 codewar

---
+++ july252022.js
@@ -0,0 +1,17 @@
+//copy pasted from previously working on the package.json for an api
+
+"name": "express-api",
+"version": "1.0.0",
+"description": "Make a simple api",
+"main": "index.js",
+"scripts": {
+  "start": "nodemon server.js",
+  "test": "echo \"Error: no test specified\" && exit 1"
+},
+"author": "",
+"license": "ISC",
+"dependencies": {
+  "cors": "^2.8.5",
+  "express": "^4.17.1",
+  "nodemon": "^2.0.7"
+}
