d7eaa174959a54e388cb8b58fa844dc8f67ac6b6
Anahi Bravo <anahi.bravo@protonmail.com>
2022-07-26 15:50:36 -0700

    july 26 codewar

---
+++ july262022.js
@@ -0,0 +1,6 @@
+function playerRankUp (points) {
+    if (points >= 100){
+      return "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up."
+    }else return false
+    }
+    // I don't know if these are becoming easier or just seeing the pattern over and over again I am able to identify it. I solved this in less than 5 min. New score for me. 
