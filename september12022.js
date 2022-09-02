95a6fb1f08d977d95f0e4f4153c24705965831ae
Anahi Bravo <anahi.bravo@protonmail.com>
2022-09-02 10:18:02 -0700

    codewar for sept 1

---
+++ september12022.js
@@ -0,0 +1,9 @@
+function remove (string) {
+    return string.split('!').join('') + "!"
+   
+   }
+   
+   /*function remove (string) {
+    let newString = string.split('!').join('')
+    return newString.push('!')
+   }*/
