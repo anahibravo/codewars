bc04f1a128e31384e6ec38ad468139fc7b90fbe6
Anahi Bravo <anahi.bravo@protonmail.com>
2022-10-01 22:26:22 -0700

    oct 1 codewar

---
+++ october12022.js
@@ -0,0 +1,20 @@
+// Add rooms here
+var rooms = {
+    bedroom: {
+      name : 'bedroom',
+      description: 'to sleep',
+      completed: true
+    },
+      
+    bathroom: {
+      name : 'bathroom',
+      description: 'to shower',
+      completed: true
+    },
+      
+    kitchen: {
+      name : 'kitchen',
+      description: 'to cook',
+      completed: true
+    }
+    }
