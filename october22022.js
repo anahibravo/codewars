0f38ac379c4fb752dc9d3af12c92b3619b823269
Anahi Bravo <anahi.bravo@protonmail.com>
2022-10-02 21:52:04 -0700

    oct 2 codewar

---
+++ october22022.js
@@ -0,0 +1,27 @@
+function greet(language) {
+
+    const otherLanguage = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+    }
+    
+    if (otherLanguage[language] !== undefined){
+      return otherLanguage[language]
+    } else return otherLanguage['english'] 
+    
+    }
