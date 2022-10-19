2b3bcd818357acde186954c7611979a86ecd6b40
anahibravo <anahi.bravo@protonmail.com>
2022-10-18 21:23:53 -0700

    oct 18 codewar

---
+++ october182022.js
@@ -0,0 +1,6 @@
+var quote = function(fighter) {
+    fighter = fighter.toLowerCase()
+    if (fighter == 'george saint pierre') {
+      return "I am not impressed by your performance."
+    } else return "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+   };
