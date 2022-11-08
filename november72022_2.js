3585a9e88d769e9321707d0a0ed53ad124cb0fc2
anahibravo <anahi.bravo@protonmail.com>
2022-11-07 23:30:15 -0800

    nov 7 second codewar

---
+++ november72022_2.js
@@ -0,0 +1,10 @@
+function points(games) {
+    let score = 0
+    for (let i=0; i < games.length; i++)
+      if (games[i][0] === games[i][2]){
+        score +=1
+      }else if (games[i][0] > games[i][2]){
+        score +=3
+      }else score +=0
+     return score
+   }
