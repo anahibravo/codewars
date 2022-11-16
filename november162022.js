a10d78f3394fb47f2def2a61a890a61807490415
anahibravo <anahi.bravo@protonmail.com>
2022-11-16 00:03:51 -0800

    nov 16 codewar not finished

---
+++ november162022.js
@@ -0,0 +1,9 @@
+String.prototype.toAlternatingCase = function () {
+  
+    let upper = this[i].spli('').toUpperCase()
+    let lower = this[i].split('').toLowerCase()
+    if ( this[i] === upper){
+      return lower.join('')
+    } else  upper.join('')
+    
+  }
