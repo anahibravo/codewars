d7945316b950a7afd48fcbfe694131d0e28121fe
anahibravo <anahi.bravo@protonmail.com>
2022-11-18 17:48:59 -0800

    nov 18 codewar

---
+++ november182022.js
@@ -0,0 +1,9 @@
+function contamination(text, char){
+    let str = ''
+      for (let i=0; i < text.length; i++){
+        str +=char
+      } return str
+     if (text === '' || char === ""){
+      return ''
+    }
+    }
