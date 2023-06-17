14899f496011ebdb0362da196128e4936ccb1657
anahibravo <anahi.bravo@protonmail.com>
2023-06-16 23:31:51 -0700

    june 16 codewar

---
+++ june162023.js
@@ -0,0 +1,13 @@
+function bmi (weight, height) {
+    let totalBmi = weight / height ** 2
+   
+  if (totalBmi <= 18.5) {
+    return "Underweight";
+  } else if (totalBmi <= 25.0){
+    return "Normal";
+  } else if (totalBmi <= 30.0) {
+    return "Overweight";
+  } else {
+     return "Obese";
+  }
+  }
