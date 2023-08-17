8595e99d783af78fb93f0b8fe7a01acbfd1f624d
anahibravo <anahi.bravo@protonmail.com>
2023-08-17 00:03:40 -0700

    agust 16 codewar fixed

--- august162023.js
+++ august162023.js
@@ -2,7 +2,7 @@ function giveMeFive(obj){
     let newarr = [];
     for (let key in obj){
       if (key.length === 5) newarr.push(key);
-      if (obj[key] === 5) newarr.push(obj[key]);
+      if (obj[key.length === 5) newarr.push(obj[key]);
       
     }
     return newarr;
